796a7a7ffe4ea23f3773ab309892d05cb7437448
unknown <tpctpc1994@gmail.com>
2022-07-31 12:58:30 +0800

    added navbar

---
+++ src/navbar/navbar.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.png"
+import './navbar.css';
+import './Gabriela-Regular.ttf';
+
+function GlamUpNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Gallery</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#link"><b>Promotions</b></Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default GlamUpNavbar;
